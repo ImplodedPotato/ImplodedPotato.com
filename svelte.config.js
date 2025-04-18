50fd626210c42223525012590a99200fee3bd75e
James Miller <cask.zingy0o@icloud.com>
2025-04-18 07:27:02 -0700

    fixed?

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,16 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+
+const dev = process.argv.includes('dev');
+
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	customElement: true,
 	kit: {
+		appDir: 'app', // Required as the default is _app
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
@@ -19,7 +23,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/ImplodedPotato.com' : '',
+			base: dev ? '' : process.env.BASE_PATH,
 		}
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.

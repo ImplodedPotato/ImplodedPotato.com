628e016e6594f7cbd686ad5c9a96386b4441f386
James Miller <cask.zingy0o@icloud.com>
2025-04-21 11:36:10 -0700

    Added deploy option

--- src/routes/seddit/main-button.svelte
+++
@@ -1,8 +0,0 @@
-<svelte:options customElement="main-button"/>
-
-<a href="https://games.implodedpotato.com">
-    <div class="main-buttons">
-        <p>Games.ImplodedPotato.com</p>
-        <p>A collection of games</p>
-    </div>
-</a>
